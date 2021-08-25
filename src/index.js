61b7bf9fade5502a4e00426338b59a078a1113d4
Yosevu Kilonzo <yosevuk@gmail.com>
2021-08-24 23:26:14 -0500

    feat: persist monsters

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+import { render } from 'uhtml'
 import { App } from './components'
-import { render } from 'uhtml';
+import { getMonsters } from './data'
+
+const DB_NAME = 'favorite-pokemon/db'
+
+const persist = (key, value) => localStorage.setItem(key, value)
+const retrieve = (key) => localStorage.getItem(key)
+
+const transformMonsters = (monsters) => {
+  return monsters.map(({
+    id: index,
+    name,
+    types,
+  }) => {
+    return {
+      index,
+      name,
+      imageUrl: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${index}.png`,
+      types: types.map(({ type }) => type.name),
+    }
+  })
+}
+
+if (retrieve(DB_NAME) === null) {
+  persist(DB_NAME, JSON.stringify(transformMonsters(await getMonsters())))
+}
+
+const monsters = JSON.parse(retrieve(DB_NAME))
 
 const initialState = {
-  monsters: [
-    { name: 'bulbasaur', liked: false },
-    { name: 'ivysaur', liked: true },
-    { name: 'venusaur', liked: false },
-  ]
+  monsters,
 }
 
 render(document.getElementById('app'), App(initialState))
