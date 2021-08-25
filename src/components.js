c3f8d5dd56e8d2df4e5966a02faebb7cb414a59f
Alice Russell <alice@heysparkbox.com>
2021-08-25 15:58:12 -0500

    feat: add likes feature

--- src/components.js
+++ src/components.js
@@ -1,15 +1,15 @@
 import { html } from 'uhtml';
 
-const likedHeart = html`
-<button class="monster__liked">
+const likedHeart = (onClick) => html`
+<button class="monster__liked" onClick=${onClick}>
   <svg xmlns="http:www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
     <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
   </svg>
 </button>
 `;
 
-const unlikedHeart = html`
-<button class="monster__unliked">
+const unlikedHeart = (onClick) => html`
+<button class="monster__unliked" onClick=${onClick}>
   <svg xmlns="http:www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
   </svg>
@@ -35,35 +35,35 @@ const MonsterTypes = (monster) => html`
 const MonsterPicture = (monster) => html`
 <img 
   class="monster__image" 
-  src=${`https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${monster.index}.png`}
+  src=${`https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${monster.id}.png`}
   alt="Bulbasaur"/>
 `;
 
-const MonsterHeart = (monster) => (monster.liked) ?  likedHeart : unlikedHeart;
+const MonsterHeart = (monster, onClick) => (monster.liked) ?  likedHeart(onClick) : unlikedHeart(onClick);
 
 const MonsterIndex = (monster) => html`
 <div class="monster__index">#00${monster.index}</div>
 `;
 
-const Monster = (monster) => html`
+const Monster = (monster, buttonClick) => html`
 <li class="monster">
   ${MonsterName(monster)}
   ${MonsterTypes(monster)}
   ${MonsterPicture(monster)}
   ${MonsterIndex(monster)}
-  ${MonsterHeart(monster)}
+  ${MonsterHeart(monster, buttonClick(monster.id-1))}
 </li>
 `;
 
-const Monsters = ({ monsters }) => html`
+const Monsters = ({ monsters, buttonClick }) => html`
 <ul class="monsters">
- ${ monsters.map((monster, index) => Monster({...monster, index: index+1}))}
+ ${ monsters.map((monster) => Monster(monster, buttonClick))}
 </ul>
 `;
 
-const App = (props) => html`
+const App = (props, buttonClick) => html`
 <div class="monsters__container">
-  ${Monsters(props)}
+  ${Monsters(props, buttonClick)}
 </div>
 `;
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import getMonsters from './data';
 
 getMonsters()
   .then(monsters => {
-    console.log(monsters);
-    render(document.getElementById('app'), App(
-      { monsters: monsters.map(monster => ({...monster, liked: false}))}
-    ));
+    const toggleLike = (monsters, i) => {
+      const newMonsters = { 
+        monsters: monsters.monsters.map(
+        monster => {
+          const thisLike = (i === monster.id-1) ? !monster.liked : monster.liked;
+          return {...monster, liked: thisLike}
+        }),
+      };
+
+      const click = buttonClick(newMonsters);
+
+      return {...newMonsters, buttonClick: click };
+    };
+
+    const buttonClick = (monsters) => (i) => () => {
+      render(document.getElementById('app'), App(toggleLike(monsters, i)));
+    };
+
+    const click = buttonClick({ monsters: monsters.map(monster => ({...monster, liked: false})) });
+    const initMonsters = {
+      monsters: monsters.map(monster => ({...monster, liked: false})),
+      buttonClick: click
+    };
+
+    render(document.getElementById('app'), App(initMonsters));
   })
   .catch(console.log);
